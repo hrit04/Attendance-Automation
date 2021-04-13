93000495d26d16497ff43350610eb1e83248a7ad
deepjyoti98 <deepjyoti98roy@gmail.com>
2021-04-13 22:22:38 +0530

    Subtle changes

--- Project/Face-Recognition-Attendance-Projects/main.py
+++ Project/Face-Recognition-Attendance-Projects/main.py
@@ -1,7 +1,12 @@
-import cv2
+#  This program implements real-time facial recognition from screen or webcam
+#  and updates them in a csv file. face_recognition library is used which is
+#  build using dlib’s deep learning based face recognition  algorithm
+#  The model has an accuracy of 99.38% on the labeled Faces in the Wild benchmark.
+   
 import numpy as np
-import face_recognition
 import os
+import face_recognition
+import cv2
 from datetime import datetime
 from PIL import ImageGrab
 
@@ -23,7 +28,7 @@ def findEncodings(images):
 
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
+        encode = face_recognition.face_encodings(img)[0]	# Only the first image
         encodeList.append(encode)
     return encodeList
 
@@ -36,30 +41,33 @@ def markAttendance(name):
         nameList = []
         for line in myDataList:
             entry = line.split(',')
-            nameList.append(entry[0])
+            nameList.append(entry[0].upper())
+            print(nameList)
             if name not in nameList:
+                print(name)
                 now = datetime.now()
                 dtString = now.strftime('%H:%M:%S')
                 f.writelines(f'\n{name},{dtString}')
 
 #### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-def captureScreen(bbox=(300,300,690+300,530+300)):
-    capScr = np.array(ImageGrab.grab(bbox))
-    capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-    return capScr
+#def captureScreen(bbox=(300,300,690+300,530+300)):
+#    capScr = np.array(ImageGrab.grab(bbox))
+#    capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+#    return capScr
 
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
 
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 while True:
-#   success, img = cap.read()
-    img = captureScreen()
+    success, img = cap.read()		# Returns a tuple  (return value, image)
+    #img = captureScreen()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS)
+    facesCurFrame = face_recognition.face_locations(imgS)	
+    # returns a list of tuples of found face locations in css(tuple) (top, right, bottom, left) order
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
@@ -78,5 +86,5 @@ while True:
             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
 
-    #cv2.imshow('Webcam', img)
+    cv2.imshow('Webcam', img)
     cv2.waitKey(1)
